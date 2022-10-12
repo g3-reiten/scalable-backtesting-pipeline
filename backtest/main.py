6d99b9ecd945c051e63e6e04a783142d145d885f
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-12 22:31:17 +0300

    backtest: making the main dynamically runnable intial steps

--- backtest/main.py
+++ backtest/main.py
@@ -6,13 +6,23 @@ import datetime
 import yfinance as yf
 #from backtrader import plot 
 import pandas as pd
+
+from datetime import datetime
+# current_time = datetime.now().strftime('%a %b %d, %Y %I:%M:%S %p')
+# print(current_time)
 class BtMain:
     
     def __init__(self) -> None:
         pass
-    def get_feeds(self, name, start_date, end_date):
+    def main_runner(self, name, start_date, end_date=None, path=None ): # accepting path to data so as to not download the data if it already exists.
+        
+        if path==None:
+            pass
+        if end_date==None:
+            end_date= datetime.strftime(datetime.now(),"%Y-%m-%d")
+            
         data_feed = yf.download(name,start_date,end_date)
-        data_feed.to_csv(f'./data/{name}-{end_date}.csv')
+        data_feed.to_csv(f'./data/{name}.csv')
         return data_feed
 
 
