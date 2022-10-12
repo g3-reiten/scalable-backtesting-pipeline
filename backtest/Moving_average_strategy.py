b5e6e308d3568e7b8d6128f02e9739502563cb8b
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-12 19:22:25 +0300

    minor modification and updatd MA strategy

--- backtest/Moving_average_strategy.py
+++ backtest/Moving_average_strategy.py
@@ -3,3 +3,31 @@ import datetime
 import yfinance as yf
 from backtrader import plot 
 
+class MA(bt.Strategy):
+    
+	def __init__(self):
+		self.ma = bt.indicators.SimpleMovingAverage(self.data.close, period=100)
+		self.order = None
+ 
+	def next(self):
+		if self.order:
+			return
+		if not self.position: # to check if we already have a position in the market
+      
+			if (self.data.close[0] > self.ma[0]) & (self.data.close[-1] < self.ma[-1]):
+				self.log('Buy Create, %.2f' % self.data.close[0])
+				self.order = self.buy(size=10) # buy when closing price today crosses above MA.
+    
+			if (self.data.close[0] < self.ma[0]) & (self.data.close[-1] > self.ma[-1]):
+				self.log('Sell Create, %.2f' % self.data.close[0])
+				self.order = self.sell(size=10)  # sell when closing price today below MA
+		else:
+		# This means we are in a position, and hence you need to define exit strategy here.
+			if len(self) >= (self.bar_executed + 4):
+				self.log('Position Closed, %.2f' % self.data.close[0])
+				self.order = self.close()
+
+	def log(self, txt):
+		dt=self.datas[0].datetime.date(0)
+		print('%s, %s' % (dt.isoformat(), txt))
+		
--- backtest/main.py
+++ backtest/main.py
@@ -1,21 +1,20 @@
+
+from __future__ import (absolute_import, division, print_function,unicode_literals)
 import backtrader as bt
 import datetime
+
 import yfinance as yf
 #from backtrader import plot 
 import pandas as pd
-class GetFeeds:
+class BtMain:
     
     def __init__(self) -> None:
         pass
     def get_feeds(self, name, start_date, end_date):
         data_feed = yf.download(name,start_date,end_date)
-  
+        data_feed.to_csv(f'./data/{name}.csv')
         return data_feed
 
-test = GetFeeds()
-    
-data = test.get_feeds('TSLA', '2018-01-01', '2019-01-01')
-data.to_csv(f'./data/TSLA.csv')
 
 
 
