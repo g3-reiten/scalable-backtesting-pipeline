72f82639628eeae02c7c26fe955b80f08685f429
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-13 18:19:45 +0300

    fix minor issues with isort

--- backtest/main.py
+++ backtest/main.py
@@ -1,16 +1,18 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-import backtrader as bt
+
 import datetime
-import yfinance as yf
-import pandas as pd
 import os
 from datetime import datetime
-import mlflow
-from mlflow import log_metric, log_param, log_artifacts, log_params
 
-from backtrader.analyzers import SharpeRatio, Returns, DrawDown, TradeAnalyzer 
+import backtrader as bt
+import mlflow
+import pandas as pd
+import yfinance as yf
+from backtrader.analyzers import DrawDown, Returns, SharpeRatio, TradeAnalyzer
 from MA_strategy import MaStrategy
+from mlflow import log_artifacts, log_metric, log_param, log_params
+
 
 class BtMain:
     """A class that sets up the cerebro and runs the backtests"""
