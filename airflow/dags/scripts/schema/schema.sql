1a574bc6a42b2f98d6c7f5a326e4b0c4607d20af
MohammedEsamaldin <mohammedesam.mem@gmail.com>
2022-10-14 15:36:42 +0200

    data

--- airflow/dags/scripts/schema/schema.sql
+++ airflow/dags/scripts/schema/schema.sql
@@ -3,12 +3,12 @@
 CREATE TABLE IF NOT EXISTS BTC
 Date,Open,High,Low,Close,Adj Close,Volume
 (
-    "Date" Date,
-    "Open" FLOAT NOT NULL,
-    "High" FLOAT NOT NULL,
-    "Low" FLOAT NOT NULL,
-    "Close" FLOAT NOT NULL,
-    "Adj close" FLOAT NOT NULL,
-    "Volume" FLOAT NOT NULL,
+    "Date" DATE NOT NULL,
+    "Open" float8 NOT NULL,
+    "High" float8 NOT NULL,
+    "Low" float8 NOT NULL,
+    "Close" float8 NOT NULL,
+    "Adj close" float8 NOT NULL,
+    "Volume" float8 NOT NULL,
     PRIMARY KEY ("Date")
 );
